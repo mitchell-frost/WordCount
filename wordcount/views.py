07fd798e3b6c6165665bc07df3337b4c2665e111
Bhawna Patnaik <b418019@iiit-bh.ac.in>
2020-08-27 22:15:18 +0530

    sorted wordcount

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
 	return render(request, 'home.html')
 
 def count(request):
-	return render(request, 'count.html')
+	fulltext = request.GET['fulltext']
+	
+	wordlist = fulltext.split()
+
+	wordDictionary = {}
+
+	for word in wordlist:
+		if word in wordDictionary:
+			wordDictionary[word] += 1
+			#Increase
+		else:
+			#add to dictionary
+			wordDictionary[word] = 1
+
+	sortedWords = sorted(wordDictionary.items(), key = operator.itemgetter(1), reverse = True)
+
+	return render(request, 'count.html', {'fulltext' : fulltext, 'count' : len(wordlist), 'sortedWords' : sortedWords})
