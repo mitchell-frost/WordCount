7d16dd18cb574b4642888cf8d252813e7348daa7
Bhawna Patnaik <b418019@iiit-bh.ac.in>
2020-08-27 23:02:50 +0530

    adding links

--- wordcount/views.py
+++ wordcount/views.py
@@ -23,3 +23,6 @@ def count(request):
 	sortedWords = sorted(wordDictionary.items(), key = operator.itemgetter(1), reverse = True)
 
 	return render(request, 'count.html', {'fulltext' : fulltext, 'count' : len(wordlist), 'sortedWords' : sortedWords})
+
+def about(request):
+	return render(request, 'about.html')
